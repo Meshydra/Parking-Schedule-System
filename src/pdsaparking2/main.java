7abac626e690b21b6a283a60a79df480974fb0e3
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-02-24 23:41:18 +0530

    Quality Fix
    Commented. Fixed Labels, Fixed UI.
    Need to work on table. And linked list. and Functions.

---
+++ src/pdsaparking2/main.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pdsaparking2;
+
+/**
+ *
+ * @author DELL
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        loadingGUI LS1 = new loadingGUI();
+        LS1.setVisible(true);
+        loadingGUI.fill();
+        LS1.setVisible(false);
+    }
+    
+}
